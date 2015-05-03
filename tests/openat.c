1f0b8d9f64c8e4d48ada32a438d9c3b81a27e869
David Roundy <roundyd@physics.oregonstate.edu>
2015-05-03 07:54:58 -0700

    use O_DIRECTORY in open call for openat test

--- tests/openat.c
+++ tests/openat.c
@@ -1,11 +1,11 @@
-#define _ATFILE_SOURCE
+#define _POSIX_C_SOURCE 200809L
 
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 
 int main() {
-  int tmpd = open("tmp", O_RDONLY);
+  int tmpd = open("tmp", O_RDONLY | O_DIRECTORY);
   openat(tmpd, "openat", O_WRONLY | O_CREAT | O_EXCL);
   return 0;
 }
