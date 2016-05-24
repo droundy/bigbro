6bd41970a70f67e343ce5a07427e4dfffa8e4cbc
David Roundy <roundyd@physics.oregonstate.edu>
2016-05-24 06:12:37 -0700

    remove unused code

--- src/linux.rs
+++ src/linux.rs
@@ -10,26 +10,60 @@ use std::os::unix;
 use std::path;
 
 fn mkstemp() -> io::Result<unix::io::RawFd> {
-    let r = try!(nix::fcntl::open(&env::temp_dir(),
+    println!("making temp");
+    let mode = nix::sys::stat::S_IRUSR | nix::sys::stat::S_IWUSR;
+    println!("mode is {:?}", mode);
+    let r = try!(nix::fcntl::open(&env::temp_dir(), // path::Path::new(&"."),
                                   nix::fcntl::O_TMPFILE
                                   | nix::fcntl::O_RDWR,
-                                  nix::sys::stat::Mode::empty()));
+                                  mode));
+    // let r = try!(nix::fcntl::open(path::Path::new(&"/tmp/test"),
+    //                               nix::fcntl::O_CREAT |
+    //                               nix::fcntl::O_RDWR,
+    //                               mode));
+    println!("done making temp");
     Ok(r)
 }
 
 pub fn shell(command_line: &str) -> io::Result<Accesses> {
+    use nix::sys::ptrace;
+    use std::ptr;
+    // let e = mkstemp();
+    // if e.is_err() {
+    //     use std::process::exit;
+    //     panic!("trouble with mkstemp {:?}", e);
+    //     exit(7);
+    // }
     match try!(nix::unistd::fork()) {
         nix::unistd::Fork::Parent(pid) => {
             use nix::sys::wait::WaitStatus::*;
             use super::ExitStatus;
-            try!(nix::unistd::setpgid(pid,pid));
+            try!(nix::unistd::setpgid(pid,pid));  // causes grandchildren to be killed along with firstborn
             println!("my child is {}", pid);
+            // Wait for the stopped process
+            println!("first wait gives {:?}",
+                     try!(nix::sys::wait::waitpid(pid,
+                                                  Some(nix::sys::wait::__WALL))));
+            println!("set options...");
+            try!(ptrace::ptrace_setoptions(pid,
+                                           ptrace::ptrace::PTRACE_O_TRACESYSGOOD |
+                                           ptrace::ptrace::PTRACE_O_TRACEFORK |
+                                           ptrace::ptrace::PTRACE_O_TRACEVFORK |
+                                           ptrace::ptrace::PTRACE_O_TRACEVFORKDONE |
+                                           ptrace::ptrace::PTRACE_O_TRACECLONE |
+                                           ptrace::ptrace::PTRACE_O_TRACEEXEC));
+            let null = ptr::null_mut();
+            println!("cont...");
+            try!(nix::sys::ptrace::ptrace(nix::sys::ptrace::ptrace::PTRACE_CONT,
+                                          pid, null, null));
+            println!("soon wait...");
             let status =
                 try!(nix::sys::wait::waitpid(pid,
                                              Some(nix::sys::wait::__WALL)));
             println!("status is {:?}", &status);
             match status {
                 Exited(_,ii) => {
+                    unimplemented!();
                     Ok(Accesses {
                         status: ExitStatus { exit_code: Some(ii as i32) },
                         read_files: HashSet::new(),
@@ -53,25 +87,22 @@ pub fn shell(command_line: &str) -> io::Result<Accesses> {
             }
         },
         nix::unistd::Fork::Child => {
-            use std::ptr;
-            try!(nix::unistd::setpgid(0,0));
+            try!(nix::unistd::setpgid(0,0)); // causes grandchildren to be killed along with firstborn
             println!("I am the child");
             try!(nix::unistd::close(0));
-            try!(nix::unistd::close(1));
-            try!(nix::unistd::close(2));
-            let stdouterrfd = try!(mkstemp());
-            try!(nix::unistd::dup2(stdouterrfd, 1));
-            try!(nix::unistd::dup2(stdouterrfd, 2));
             try!(nix::fcntl::open(path::Path::new(&"/dev/null"),
                                   nix::fcntl::O_RDONLY,
                                   nix::sys::stat::Mode::empty()));
-            let p1 = ptr::null_mut();
-            let p2 = ptr::null_mut();
+            // try!(nix::unistd::close(1));
+            // try!(nix::unistd::close(2));
+            // try!(nix::unistd::dup2(stdouterrfd, 1));
+            // try!(nix::unistd::dup2(stdouterrfd, 2));
+            let null = ptr::null_mut();
             try!(nix::sys::ptrace::ptrace(nix::sys::ptrace::ptrace::PTRACE_TRACEME,
-                                          0, p1, p2));
-            // let myself = nix::unistd::getpid();
-            // try!(nix::sys::signal::kill(myself,
-            //                             nix::sys::signal::signal::SIGSTOP));
+                                          0, null, null));
+            let myself = nix::unistd::getpid();
+            try!(nix::sys::signal::kill(myself,
+                                        nix::sys::signal::signal::SIGSTOP));
             try!(nix::unistd::execvp(&CString::new("sh").unwrap(),
                                      &[CString::new("sh").unwrap(),
                                        CString::new("-c").unwrap(),
