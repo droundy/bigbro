907c4a142388df7792f20691ffecddd37bbdb614
David Roundy <roundyd@physics.oregonstate.edu>
2016-06-02 12:44:52 -0700

    add a bunch of copyright headers (also note that there is nothing left of fsatrace in bigbro-windows.c)

--- fileaccesses.c
+++ fileaccesses.c
@@ -1,5 +1,5 @@
 /* bigbro filetracking library
-   Copyright (C) 2015 David Roundy
+   Copyright (C) 2015,2016 David Roundy
 
    This program is free software; you can redistribute it and/or
    modify it under the terms of the GNU General Public License as
