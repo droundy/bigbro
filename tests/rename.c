0905ee7f41303ac190170fe1a23b37d40d1d7fb6
David Roundy <roundyd@physics.oregonstate.edu>
2016-06-28 08:30:15 -0700

    make rename.c test first create file to rename

--- tests/rename.c
+++ tests/rename.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 
 int main(void) {
+  FILE *f = fopen("tmp/foodle", "a");
+  fprintf(f, "hi there!\n");
+  fclose(f);
   fprintf(stderr, "about to rename\n");
-  int returncode = rename("tmp/foo", "tmp/barbaz");
+  int returncode = rename("tmp/foodle", "tmp/barbaz");
   fprintf(stderr, "rename gives %d\n", returncode);
   return 0;
 }
