c8172444672ba8d0bdcd3aac0c2c489738867545
David Roundy <roundyd@physics.oregonstate.edu>
2017-03-22 13:51:55 -0700

    disable hooking in windows version of bigbro

--- bigbro-windows.c
+++ bigbro-windows.c
@@ -53,38 +53,38 @@ int bigbro(const char *workingdir, pid_t *child_ptr,
   *mkdir_directories = malloc(sizeof(char *));
   **mkdir_directories = 0;
   create_dlls();
-  struct queue q;
-  const char *shm_name = "stupid"; // fixme: need to generate unique name
-  if (queueInit(&q, shm_name)) {
-    printf("Error allocating shared memory.\n");
-    return 1;
-  }
+  /* struct queue q; */
+  /* const char *shm_name = "stupid"; // fixme: need to generate unique name */
+  /* if (queueInit(&q, shm_name)) { */
+  /*   printf("Error allocating shared memory.\n"); */
+  /*   return 1; */
+  /* } */
   if (envp) {
     // create an environment with the desired environment variables,
     // plus one more to carry the name of our shared-memory segment.
     // This is trickier than on posix, since windows wants a
     // null-separated array of char, rather than a null-terminated
     // array of char *.  For uniformity, bigbro wants the latter.
-    int size = 1; // 1 for the final null
-    for (char **e = envp; *e; e++) {
-      size += strlen(*e) + 1;
-    }
-    size += strlen("bigbro_shm=") + strlen(shm_name) + 1;
-    char *new_windows_env = (char *)calloc(size, 1);
-    // here we join together all these strings into one big happy
-    // family.
-    char *location = new_windows_env;
-    for (char **e = envp; *e; e++) location = copy_string(location, *e);
-    location = copy_string(location, "bigbro_shm=");
-    location--; // overwrite the null
-    location = copy_string(location, shm_name);
+    /* int size = 1; // 1 for the final null */
+    /* for (char **e = envp; *e; e++) { */
+    /*   size += strlen(*e) + 1; */
+    /* } */
+    /* size += strlen("bigbro_shm=") + strlen(shm_name) + 1; */
+    /* char *new_windows_env = (char *)calloc(size, 1); */
+    /* // here we join together all these strings into one big happy */
+    /* // family. */
+    /* char *location = new_windows_env; */
+    /* for (char **e = envp; *e; e++) location = copy_string(location, *e); */
+    /* location = copy_string(location, "bigbro_shm="); */
+    /* location--; // overwrite the null */
+    /* location = copy_string(location, shm_name); */
   } else {
     // FIXME BUG HERE! the following introduces a race condition: if
     // we call bigbro twice simultaneously, it is possible that one of
     // the two processes will end up sending its info to the wrong
     // shm.  However, right now I am just trying to get something
     // working, and this is easier.
-    SetEnvironmentVariable("bigbro_shm", shm_name);
+    /* SetEnvironmentVariable("bigbro_shm", shm_name); */
   }
 
   STARTUPINFO si;
@@ -95,11 +95,10 @@ int bigbro(const char *workingdir, pid_t *child_ptr,
   si.hStdError = stderrfd;
   si.hStdOutput = stdoutfd;
 
-  // want to pass shm_name value in the environment...
   if (!CreateProcess(0, (char *)cmdline, 0, 0, 0, CREATE_SUSPENDED, 0, 0, &si, &pi)) {
     return -1;
   }
-  injectProcess(pi.hProcess);
+  /* injectProcess(pi.hProcess); */
   if (ResumeThread(pi.hThread) == -1) {
     printf("I got trouble ResumeThreading\n");
     return -1;
@@ -116,109 +115,109 @@ int bigbro(const char *workingdir, pid_t *child_ptr,
   if (!CloseHandle(pi.hThread) || !CloseHandle(pi.hProcess)) {
     return 1;
   }
-  hashset read, readdir, written;
-  init_hashset(&read, 1024);
-  init_hashset(&readdir, 1024);
-  init_hashset(&written, 1024);
-  for (uint32_t i=0; i<q.buf->written_to_here; i+= strlen(q.buf->data+i)+1) {
-    const char *name = q.buf->data+i+1;
-    switch (q.buf->data[i]) {
-    case WRITE_OP:
-      insert_hashset(&written, name);
-      delete_from_hashset(&read, name);
-      delete_from_hashset(&readdir, name);
-      break;
-    case READ_OP:
-      if (!lookup_in_hash(&written, name)) {
-        insert_hashset(&read, name);
-      }
-      break;
-    case RENAME_OP:
-      i += strlen(q.buf->data+i)+1;
-      const char *from = name;
-      const char *absto = q.buf->data+i;
-      printf("handling rename %s -> %s\n", from, absto);
+  /* hashset read, readdir, written; */
+  /* init_hashset(&read, 1024); */
+  /* init_hashset(&readdir, 1024); */
+  /* init_hashset(&written, 1024); */
+  /* for (uint32_t i=0; i<q.buf->written_to_here; i+= strlen(q.buf->data+i)+1) { */
+  /*   const char *name = q.buf->data+i+1; */
+  /*   switch (q.buf->data[i]) { */
+  /*   case WRITE_OP: */
+  /*     insert_hashset(&written, name); */
+  /*     delete_from_hashset(&read, name); */
+  /*     delete_from_hashset(&readdir, name); */
+  /*     break; */
+  /*   case READ_OP: */
+  /*     if (!lookup_in_hash(&written, name)) { */
+  /*       insert_hashset(&read, name); */
+  /*     } */
+  /*     break; */
+  /*   case RENAME_OP: */
+  /*     i += strlen(q.buf->data+i)+1; */
+  /*     const char *from = name; */
+  /*     const char *absto = q.buf->data+i; */
+  /*     printf("handling rename %s -> %s\n", from, absto); */
 
-      if (lookup_in_hash(&written, from) || lookup_in_hash(&read, from)) {
-        // Looks like it might be a file!
-        delete_from_hashset(&written, from);
-        delete_from_hashset(&read, from);
-        insert_hashset(&written, absto);
-      } else {
-        // I think it might be a directory
-        char *fromslash = malloc(strlen(from)+2);
-        strcpy(fromslash, from);
-        strcat(fromslash, "\\");
-        char *toslash = malloc(strlen(absto)+2);
-        strcpy(toslash, absto);
-        strcat(toslash, "\\");
-        int fromslashlen = strlen(fromslash);
-        int toslashlen = strlen(toslash);
-        {
-          hashset new_written;
-          init_hashset(&new_written, 2*read.num_entries);
-          for (struct hash_entry *e = written.first; e; e = e->next) {
-            if (strncmp(e->key, fromslash, fromslashlen) == 0) {
-              char *newk = malloc(strlen(e->key) - fromslashlen + toslashlen + 1);
-              strcpy(newk, toslash);
-              strcat(newk, e->key + fromslashlen);
-              printf("need to rename %s to %s\n", e->key, newk);
-              insert_hashset(&new_written, newk);
-              /* insert_hashset(&written, newk); */
-              /* delete_from_hashset(&written, e->key); */
-            } else {
-              insert_hashset(&new_written, e->key);
-            }
-          }
-          hashset new_read;
-          init_hashset(&new_read, 2*read.num_entries);
-          for (struct hash_entry *e = read.first; e; e = e->next) {
-            if (strncmp(e->key, fromslash, fromslashlen) == 0) {
-              char *newk = malloc(strlen(e->key) - fromslashlen + toslashlen + 1);
-              strcpy(newk, toslash);
-              strcat(newk, e->key + fromslashlen);
-              printf("need to rename %s to %s\n", e->key, newk);
-              insert_hashset(&new_written, newk);
-            /* insert_hashset(&written, newk); */
-            /* delete_from_hashset(&read, e->key); */
-            } else {
-              insert_hashset(&new_read, e->key);
-            }
-          }
-          free_hashset(&written);
-          free_hashset(&read);
-          written = new_written;
-          read = new_read;
-        }
-        /* for (struct hash_entry *e = readdir.first; e; e = e->next) { */
-        /*   if (strncmp(e->key, fromslash, fromslashlen) == 0) { */
-        /*     delete_from_hashset(&readdir, e->key); */
-        /*   } */
-        /* } */
-      }
-      break;
-    case READDIR_OP:
-      insert_hashset(&readdir, name);
-      break;
-    case GETINFO_OP:
-      insert_hashset(&read, name);
-      break;
-    case SETINFO_OP:
-      insert_hashset(&written, name);
-      break;
-    default:
-      printf("BUG: I do not know why '%c' shows up!\n", q.buf->data[i]);
-    }
-    printf("%c -> %s\n",q.buf->data[i], &q.buf->data[i+1]);
-  }
-  free(*read_from_files);
-  free(*read_from_directories);
-  free(*written_to_files);
-  *read_from_files = hashset_to_array(&read);
-  *read_from_directories = hashset_to_array(&readdir);
-  *written_to_files = hashset_to_array(&written);
-  free_hashset(&read);
-  free_hashset(&readdir);
-  free_hashset(&written);
+  /*     if (lookup_in_hash(&written, from) || lookup_in_hash(&read, from)) { */
+  /*       // Looks like it might be a file! */
+  /*       delete_from_hashset(&written, from); */
+  /*       delete_from_hashset(&read, from); */
+  /*       insert_hashset(&written, absto); */
+  /*     } else { */
+  /*       // I think it might be a directory */
+  /*       char *fromslash = malloc(strlen(from)+2); */
+  /*       strcpy(fromslash, from); */
+  /*       strcat(fromslash, "\\"); */
+  /*       char *toslash = malloc(strlen(absto)+2); */
+  /*       strcpy(toslash, absto); */
+  /*       strcat(toslash, "\\"); */
+  /*       int fromslashlen = strlen(fromslash); */
+  /*       int toslashlen = strlen(toslash); */
+  /*       { */
+  /*         hashset new_written; */
+  /*         init_hashset(&new_written, 2*read.num_entries); */
+  /*         for (struct hash_entry *e = written.first; e; e = e->next) { */
+  /*           if (strncmp(e->key, fromslash, fromslashlen) == 0) { */
+  /*             char *newk = malloc(strlen(e->key) - fromslashlen + toslashlen + 1); */
+  /*             strcpy(newk, toslash); */
+  /*             strcat(newk, e->key + fromslashlen); */
+  /*             printf("need to rename %s to %s\n", e->key, newk); */
+  /*             insert_hashset(&new_written, newk); */
+  /*             /\* insert_hashset(&written, newk); *\/ */
+  /*             /\* delete_from_hashset(&written, e->key); *\/ */
+  /*           } else { */
+  /*             insert_hashset(&new_written, e->key); */
+  /*           } */
+  /*         } */
+  /*         hashset new_read; */
+  /*         init_hashset(&new_read, 2*read.num_entries); */
+  /*         for (struct hash_entry *e = read.first; e; e = e->next) { */
+  /*           if (strncmp(e->key, fromslash, fromslashlen) == 0) { */
+  /*             char *newk = malloc(strlen(e->key) - fromslashlen + toslashlen + 1); */
+  /*             strcpy(newk, toslash); */
+  /*             strcat(newk, e->key + fromslashlen); */
+  /*             printf("need to rename %s to %s\n", e->key, newk); */
+  /*             insert_hashset(&new_written, newk); */
+  /*           /\* insert_hashset(&written, newk); *\/ */
+  /*           /\* delete_from_hashset(&read, e->key); *\/ */
+  /*           } else { */
+  /*             insert_hashset(&new_read, e->key); */
+  /*           } */
+  /*         } */
+  /*         free_hashset(&written); */
+  /*         free_hashset(&read); */
+  /*         written = new_written; */
+  /*         read = new_read; */
+  /*       } */
+  /*       /\* for (struct hash_entry *e = readdir.first; e; e = e->next) { *\/ */
+  /*       /\*   if (strncmp(e->key, fromslash, fromslashlen) == 0) { *\/ */
+  /*       /\*     delete_from_hashset(&readdir, e->key); *\/ */
+  /*       /\*   } *\/ */
+  /*       /\* } *\/ */
+  /*     } */
+  /*     break; */
+  /*   case READDIR_OP: */
+  /*     insert_hashset(&readdir, name); */
+  /*     break; */
+  /*   case GETINFO_OP: */
+  /*     insert_hashset(&read, name); */
+  /*     break; */
+  /*   case SETINFO_OP: */
+  /*     insert_hashset(&written, name); */
+  /*     break; */
+  /*   default: */
+  /*     printf("BUG: I do not know why '%c' shows up!\n", q.buf->data[i]); */
+  /*   } */
+  /*   printf("%c -> %s\n",q.buf->data[i], &q.buf->data[i+1]); */
+  /* } */
+  /* free(*read_from_files); */
+  /* free(*read_from_directories); */
+  /* free(*written_to_files); */
+  /* *read_from_files = hashset_to_array(&read); */
+  /* *read_from_directories = hashset_to_array(&readdir); */
+  /* *written_to_files = hashset_to_array(&written); */
+  /* free_hashset(&read); */
+  /* free_hashset(&readdir); */
+  /* free_hashset(&written); */
   return dword_return_code;
 }
