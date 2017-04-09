0eb9e45af8abfb56fc7df4a91b47fe0db05ad9cf
David Roundy <roundyd@physics.oregonstate.edu>
2017-04-09 08:15:56 -0700

    test cross-compiling on gitlab

--- build/test.sh
+++ build/test.sh
@@ -15,6 +15,15 @@ git config --global user.name "CI Builder"
 git clone git://github.com/droundy/bigbro
 cd bigbro
 
+cargo build --features strict
+cargo test --features strict
+cargo doc --features strict
+# cargo build --features strict --target x86_64-apple-darwin
+cargo build --features strict --target i686-pc-windows-gnu
+cargo build --features strict --target x86_64-pc-windows-gnu
+
+rm -rf target
+
 sh build/linux.sh
 python3 run-tests.py -v
 
