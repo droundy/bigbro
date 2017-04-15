06d72d61c66c481ba5db897a939ea0b7f5f0a65f
David Roundy <roundyd@physics.oregonstate.edu>
2017-04-15 07:05:11 -0700

    fix docker builds, switching to stretch so we can cross-compile to windows

--- build/test.sh
+++ build/test.sh
@@ -19,13 +19,24 @@ cargo build --features strict
 cargo test --features strict
 cargo doc --features strict
 # cargo build --features strict --target x86_64-apple-darwin
-cargo build --features strict --target i686-pc-windows-gnu
+# cargo build --features strict --target i686-pc-windows-gnu
 cargo build --features strict --target x86_64-pc-windows-gnu
 
 rm -rf target
 
 sh build/linux.sh
 python3 run-tests.py -v
+
+# Now testing that website can be built...
+
+rm -rf target Cargo.lock
+git clone https://gitlab.com/facio/fac.git tests/fac
+cd tests/fac
+sh build/linux.sh
+./fac
+cd ../..
+cargo --version
+tests/fac/fac web/style.css web/documentation.html web/doc/bigbro/index.html web/index.html
 
 # execute this with:
 # docker build -t facio/bigbro .
