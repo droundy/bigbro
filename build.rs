4ad20cedf0357bc1d2896f63bceaed64e0e70db8
David Roundy <roundyd@physics.oregonstate.edu>
2017-07-16 06:34:09 -0700

    do not bother trying to run python in build.rs, just use linux.h from repository

--- build.rs
+++ build.rs
@@ -1,6 +1,5 @@
 extern crate gcc;
 
-use std::os::unix::io::{FromRawFd, IntoRawFd};
 use std::env;
 
 fn main() {
@@ -14,15 +13,6 @@ fn main() {
             .include(".")
             .compile("libbigbro.a");
     } else if target.contains("linux") {
-        if ! std::path::Path::new("syscalls/linux.h").exists() {
-            let linux_h_fd = std::fs::File::create("syscalls/linux.h")
-                .unwrap().into_raw_fd();
-            std::process::Command::new("python3")
-                .args(&["syscalls/linux.py"])
-                .stdout(unsafe {std::process::Stdio::from_raw_fd(linux_h_fd)})
-                .status().unwrap();
-        }
-
         gcc::Config::new()
             .flag("-std=c99")
             .file("bigbro-linux.c")
