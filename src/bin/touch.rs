cd64c7e7da5f914dac6f6b7224d0542c5c116f1d
David Roundy <roundyd@physics.oregonstate.edu>
2019-07-21 14:32:39 -0700

    remove executables intended only for enabling test scripts to run on windows (but unused)

--- src/bin/touch.rs
+++
@@ -1,9 +0,0 @@
-fn main() {
-    let mut it = std::env::args();
-    it.next();
-    for arg in it {
-        println!("touching {:?}", &arg);
-        std::fs::OpenOptions::new().create(true).append(true).open(arg)
-            .expect("trouble touching file");
-    }
-}
